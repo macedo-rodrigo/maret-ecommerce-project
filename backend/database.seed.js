81dd929e4eb9473aec13b6034c637755c22f3162
Rodri <rodrigoalvesdemacedo@live.com>
2024-08-06 00:18:02 +0200

    express error handler

--- backend/database.seed.js
+++ backend/database.seed.js
@@ -1,23 +1,28 @@
 const router = require("express").Router();
-
+const AsyncHandler = require("express-async-handler");
 // Models
 const User = require("./models/User");
 const Product = require("./models/Product");
-
 // Data
 const usersData = require("./data/Users");
 const productsData = require("./data/Products");
 
-router.post("/users", async (req, res) => {
-  await User.deleteMany({});
-  const userSeeder = await User.insertMany(usersData);
-  res.send({userSeeder});
-});
+router.post(
+  "/users",
+  AsyncHandler(async (req, res) => {
+    await User.deleteMany({});
+    const userSeeder = await User.insertMany(usersData);
+    res.send({ userSeeder });
+  })
+);
 
-router.post("/products", async (req, res) => {
-  await Product.deleteMany({});
-  const productSeeder = await Product.insertMany(productsData);
-  res.send({productSeeder});
-});
+router.post(
+  "/products",
+  AsyncHandler(async (req, res) => {
+    await Product.deleteMany({});
+    const productSeeder = await Product.insertMany(productsData);
+    res.send({ productSeeder });
+  })
+);
 
 module.exports = router;
