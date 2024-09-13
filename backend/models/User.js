fca3df1b0188390fa3421006c78dd08e1bc85f1d
Rodri <rodrigoalvesdemacedo@live.com>
2024-09-14 00:43:26 +0200

    register route and register password middleware finished

--- backend/models/User.js
+++ backend/models/User.js
@@ -18,4 +18,14 @@ userSchema.methods.matchPassword = async function (enterPassword) {
   return await bcrypt.compare(enterPassword, this.password);
 };
 
+// register password hash and store
+userSchema.pre("save", async function (next) {
+  if(!this.isModified('password')){
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+})
+
 module.exports = mongoose.model("User", userSchema);
